e5e62102db3ceb9a8332a55366ed58261b0b1ec2
pathakakshay <pathakakshay1111@gmail.com>
2020-09-26 19:36:03 +0530

    front page

--- themes/custom/vmsit/js/scripts.js
+++ themes/custom/vmsit/js/scripts.js
@@ -12,11 +12,22 @@ jQuery(document).ready(function(){
     jQuery(".viewless").hide(1000);
     jQuery(".viewmore").show();
   });
-  jQuery('.slick-carousel').slick({
+  jQuery('.slick-product-carousel').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplayspeed: 1000,
+    variableWidth: false,
+    speed: 1000,
+  });
+  jQuery('.slick-categories-carousel').slick({
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 8,
+    dots: false,
     arrows: true,
     autoplay: true,
     autoplayspeed: 1000,
